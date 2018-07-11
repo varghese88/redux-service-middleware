65b656abf96ca7a2fc113430d1842280a7274edc
varghesejohn88@gmail.com <vargu8229>
2018-07-10 23:07:03 -0400

    new feature promise and DI handling in payload

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 import axios from 'axios';
-export const dataService = 
-    ({ dependencies, config}) => 
-        ({dispatch, getState}) => 
-            next =>
-                 action => {
+export const dataService =
+({ dependencies, config}) =>
+    ({dispatch, getState}) =>
+        next =>
+            action => {
 
     const { type, payload={}, process, validate} = action;
     const { baseConfig, serviceConfig } = config || {};
     const url = serviceConfig && serviceConfig[type] ? serviceConfig[type]['url'] : '';
     const method = serviceConfig && serviceConfig[type] ? serviceConfig[type]['method'] : '';
+    const isPayloadFn = payload && typeof payload === 'function';
+    const content =  isPayloadFn? payload({...dependencies}): payload;
+    const isPayloadPromise = content && (typeof content === 'object' || typeof content === 'function') && typeof content.then === 'function';
 
+    const promiseSuccess = response => {
+        next({ type:`${type}_FULFILLED`, payload:response});
+    };
+
+    const promiseFailure = error => {
+        next({
+            type:`${type}_REJECTED`,
+            payload:error
+        })
+    };
     const extractPromiseData = promise => {
         next({ type:`${type}_PENDING`});
-        promise.then( response => response.data)
-            .then( response => {
-                next({ type:`${type}_FULLFILED`, payload:response});
-            })
-            .catch( error => {
-                next({
-                    type:`${type}_REJECTED`,
-                    payload:error
-                })
-            });
-    }
-    
+        promise.then( response => response.data).then(promiseSuccess).catch(promiseFailure);
+    };
+
     const postApiGenerator = () => {
         return axios.post(url, {
             ...baseConfig,
             method,
-            data:payload
+            data:content
         });
     };
-    
+
     const getApiGenerator = () => {
         return axios.get(url, {
             ...baseConfig,
             method,
-            params:payload
+            params:content
         })
     };
     const accept = () => {
@@ -49,12 +53,13 @@ export const dataService =
                 break;
             default:
         }
+        isPayloadPromise && content.then(promiseSuccess).catch(promiseFailure);
         if(process){
             process({...dependencies, dispatch, getState})
         } else {
             next(action);
         }
-    }
+    };
     const reject = () =>{};
 
     if(validate) {
